f1f782eb1dfbb8c84aa9321bd69af39cacbc9432
Chris McClimans <chris@hippiehacker.org>
2012-01-17 08:52:39 +1300

    make log statements a bit more visible

--- powershell/libraries/helper.rb
+++ powershell/libraries/helper.rb
@@ -34,8 +34,10 @@ module Powershell
     end
 
     def interpreter
+      Chef::Log.debug "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
       Chef::Log.debug %x[dir #{File.dirname("#{ENV['WINDIR']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe")}]
       Chef::Log.debug %x[dir #{File.dirname("#{ENV['WINDIR']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe")}]
+      Chef::Log.debug "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
       # force 64-bit powershell from 32-bit ruby process
       if ::File.exist?("#{ENV['WINDIR']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe")
         "#{ENV['WINDIR']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe"
